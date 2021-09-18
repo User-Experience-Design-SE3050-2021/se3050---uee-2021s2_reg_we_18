868d4f1504b46051890862abec17c41a70dbf494
pasindu <it19390260@my.sliit.lk>
2021-09-19 01:27:45 +0530

    signout

---
+++ riyapola-mobApp/riyapola/screens/signup.js
@@ -0,0 +1,56 @@
+// import React from 'react';
+// import { View, Text } from 'react-native';
+// import Tabs from '../shared/Tabs';
+// import { globalStyles } from '../styles/global';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+// import { Input } from 'react-native-elements';
+// export default function signup() {
+//     return (
+//         <View>
+//             <Tabs pageIndex={1} />
+//             <View style={globalStyles.container}>
+//                 {/* <Text style={globalStyles.titleText}>All Ads screen</Text> */}
+//                 <Input
+//                     placeholder='BASIC INPUT'
+//                 />
+
+//                 <Input
+//                     placeholder='INPUT WITH ICON'
+//                     leftIcon={{ type: 'font-awesome', name: 'chevron-left' }}
+//                 />
+
+//                 <Input
+//                     placeholder='INPUT WITH CUSTOM ICON'
+//                     leftIcon={
+//                         <Icon
+//                             name='user'
+//                             size={24}
+//                             color='black'
+//                         />
+//                     }
+//                 />
+
+
+//                 <Input
+//                     placeholder="Comment"
+//                     leftIcon={{ type: 'font-awesome', name: 'comment' }}
+//                     style={styles}
+//                     onChangeText={value => this.setState({ comment: value })}
+//                 />
+
+
+//                 <Input
+//                     placeholder='INPUT WITH ERROR MESSAGE'
+//                     errorStyle={{ color: 'red' }}
+//                     errorMessage='ENTER A VALID ERROR HERE'
+//                 />
+
+//                 <Input placeholder="Password" secureTextEntry={true} />
+//             </View>
+//         </View>
+
+//     )
+// }
+
+
+
